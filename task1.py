2b8a4e936be68aa4b4a7c18315bd3b72ce198de8
mariashubna <mariashubna@gmail.com>
2024-05-04 14:27:37 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,41 @@
+# import timeit
 import random
-len_num = [10, 100, 1000]
-for len in len_num:
-    num = random.randint(0,len)
-    print(num)
+
+def random_list(length):
+    return [random.randint(0, 1000) for _ in range(length)]
+
+def merge_sort(number_len):
+    arr = random_list(number_len)
+    
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+   
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
+num_len = [10, 100, 1000]
+for len in num_len:
+    print(merge_sort(len))
