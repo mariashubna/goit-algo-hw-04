f3be4b4bd8993ab4fe6b0e9612423ecfb0c7ecc1
mariashubna <mariashubna@gmail.com>
2024-05-04 16:06:45 +0200

    done1

--- task1.py
+++ task1.py
@@ -1,17 +1,21 @@
-# import timeit
 import random
+import timeit
 
+# Список рандломних значень
 def random_list(length):
     return [random.randint(0, 1000) for _ in range(length)]
 
-def merge_sort(number_len):
-    arr = random_list(number_len)
+# Сортування злиття
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
     
     mid = len(arr) // 2
     left_half = arr[:mid]
     right_half = arr[mid:]
 
     return merge(merge_sort(left_half), merge_sort(right_half))
+
 
 def merge(left, right):
     merged = []
@@ -36,6 +40,29 @@ def merge(left, right):
 
     return merged
 
-num_len = [10, 100, 1000]
-for len in num_len:
-    print(merge_sort(len))
+# Сортування вставками
+def insertion_sort(arr): 
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key 
+    return arr
+
+# Кількість чисел у масиві для сортування
+num_lengths = [10, 30, 50, 80, 100, 500, 1000]
+
+for length in num_lengths:
+    arr = random_list(length)
+    merge_sort_time = timeit.timeit(stmt='merge_sort(arr.copy())', globals=globals(), number=100)
+    print(f"Час для сортування злиттям для кількості чисел {length}: {merge_sort_time}")
+    insertion_sort_time = timeit.timeit(stmt='insertion_sort(arr.copy())', globals=globals(), number=100)
+    print(f"Час для сортування вставками для кількості чисел {length}: {insertion_sort_time}")
+    timsort_time = timeit.timeit(stmt='sorted(arr.copy())', globals=globals(), number=100)
+    print(f"Час для сортування Timsort для кількості чисел {length}: {timsort_time}")
+    print('-------------------------------------------------------------------------')
+
+
+
